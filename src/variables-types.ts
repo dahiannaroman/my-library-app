0bd04a82d24810ab8f6e3961dc21b049b415c0c1
Karen Roman <karendahiannaromanbarrios@gmail.com>
2024-06-13 07:46:30 -0500

    tipado primitivo

---
+++ src/variables-types.ts
@@ -0,0 +1,22 @@
+let bookTitle: string = "TypeScript Basics";
+let libraryName: string = "My Library";
+let numberOfbooks: number = 13;
+let isLibraryOpen: boolean = true;
+
+//Libro
+let titulo: string = "El nombre del viento";
+let autor: string = "Patrick Rothfuss";
+let genero: string = "Ficción";
+let idioma: string = "Español";
+let precio: number = 20.50;
+let formato: string = "Tapa blanda";
+let isbn: string = "978-8401338086";
+let descripcion: string = "Novela de fantasía";
+let estado: string = "Nuevo";
+let ubicacion: string = "Estantería A-5";
+let fecha_publicacion: Date = new Date("2007-03-27");
+let editorial: string = "Plaza y Janés Editorial";
+let paginas: number = 832;
+let dimensiones: [number, number, number] = [15, 4, 23]; 
+let peso: number = 750;
+let disponible: boolean = true;
